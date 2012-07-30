beea1cc29ce20e7b4ca78b10c5f7245aad8545f3
Gankro <a.beingessner@gmail.com>
2012-07-30 19:01:12 -0400

    actually added the production script, is retarded

---
+++ production.sh
@@ -0,0 +1,11 @@
+cat Jterniabound.js Sprite.js Fighter.js Character.js SpriteButton.js Animation.js Room.js FontEngine.js Action.js Trigger.js commands.js serialization.js Dialoger.js Chooser.js Audio.js Assets.js Path.js > Sburb.js
+./jsmin < Sburb.js > Sburb.min.js
+cp index.html index_dev.html
+rm index.html
+cp index_production.html index.html
+zip -r Jterniabound.zip Sburb.min.js index.html README-ownership-liscensing.txt resources levels 
+rm Sburb.js
+rm Sburb.min.js
+rm index.html
+cp index_dev.html index.html
+rm index_dev.html
